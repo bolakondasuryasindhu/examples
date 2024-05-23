ef8d8d638927aa41855bba8557b150bc99ad346c
bolakondasuryasindhu <168550731+bolakondasuryasindhu@users.noreply.github.com>
2024-05-23 15:14:37 +0530

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ASCII {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a character: ");
+        
+        char character = scanner.next().charAt(0);
+        
+        int asciiValue = (int) character;
+        
+        System.out.println("The ASCII value of " + character + " is " + asciiValue);
+        
+        scanner.close();
+    }
+}
