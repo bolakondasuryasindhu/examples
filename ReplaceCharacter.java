4d463d0153fae1b0561362c71a3a1a7c04630fff
bolakondasuryasindhu <168550731+bolakondasuryasindhu@users.noreply.github.com>
2024-05-23 15:17:10 +0530

    Add files via upload

---
+++ ReplaceCharacter.java
@@ -0,0 +1,13 @@
+public class ReplaceCharacter {
+    public static void main(String[] args) {
+        // Define the original string
+        String originalString = "Hello Everyone";
+
+        // Replace all occurrences of 'e' with 'a'
+        String modifiedString = originalString.replace('e', 'a');
+
+        // Output the modified string
+        System.out.println("Original string: " + originalString);
+        System.out.println("Modified string: " + modifiedString);
+    }
+}
